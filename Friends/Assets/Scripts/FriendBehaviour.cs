d227a1406364ad33e8553ffa49a3c8ce2fc45747
Andrew Seba <sebaturtle@gmail.com>
2017-01-24 18:40:45 -0600

    First Commit

---
+++ Friends/Assets/Scripts/FriendBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// How the friend behaves.
+/// </summary>
+public class FriendBehaviour : MonoBehaviour {
+
+    Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerDir = player.position - transform.position;
+        if(playerDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(playerDir, Vector3.up);
+            transform.rotation = Quaternion.Euler(1, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        }
+    }
+}
---
+++ Friends/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Player controller.
+/// </summary>
+public class PlayerController : MonoBehaviour {
+
+    [Tooltip("How fast the player can move.")]
+    public float speed = 0.5f;
+    public float turnSpeed = 0.1f;
+    float horizontal;
+    float vertical;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Horizontal Input
+        horizontal = Input.GetAxis("Horizontal");
+        //Vertical Input
+        vertical = Input.GetAxis("Vertical");
+
+        #region MoveToFixed
+        Vector3 input = new Vector3(horizontal, 0, vertical);
+
+        //Stop the player from speeding up moving diagnally
+        if (input.magnitude > 1)
+        {
+            input = input.normalized;
+        }
+        if (input != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(input);
+
+        }
+
+        //Apply input.
+        transform.position += (input * speed) * Time.deltaTime;
+        #endregion
+        
+	}
+
+    /// <summary>
+    /// Do physics stuff here.
+    /// </summary>
+    void FixedUpdate()
+    {
+
+        
+    }
+
+    
+}
