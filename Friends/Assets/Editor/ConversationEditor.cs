2b7b757bc65d8470d8b58e8b304a6a051082b5ae
Andrew Seba <sebaturtle@gmail.com>
2017-01-27 17:03:38 -0600

    Conversation Editor first commit
    editor window opens.

---
+++ Friends/Assets/Editor/ConversationEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+/// <summary>
+/// Author: Andrew Seba 
+/// Description: Opens the conversation editor window.
+/// </summary>
+public class ConversationEditor : MonoBehaviour {
+
+    [MenuItem("Friend Tools/Conversation Editor")]
+	public static void OpenConversationEditor()
+    {
+        ConversationEditorWindow window = (ConversationEditorWindow)EditorWindow.GetWindow(typeof(ConversationEditorWindow));
+        window.Show();
+    }
+}
---
+++ Friends/Assets/Editor/ConversationEditorWindow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/// <summary>
+/// Conversation editor to easily create conversations
+/// Questions and item transfers.
+/// </summary>
+public class ConversationEditorWindow : EditorWindow {
+
+
+
+	void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("New"))
+        {
+            if(EditorUtility.DisplayDialog("Create a new conversation?",
+                "You will lose any unsaved changes.", "Ok", "Cancel"))
+            {
+                //Reset local variables.
+
+                this.Repaint();
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Label("Conversation");
+
+        if(GUILayout.Button("Save Conversation"))
+        {
+            Debug.Log("Feature Incomplete.");
+        }
+    }
+}
